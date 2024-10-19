4f4f576df4e2db1316fcdae163bf4bf8d9a881d1
NagaVaishnavi-Puppala <vaishnavi.puppala@peopletech.com>
2024-10-19 16:40:13 +0530

    updated .env and websocket

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { BlockMetrics, HistoricalDataPoint } from '../types';
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = process.env.REACT_APP_BACKEND_API_URL || 'http://34.210.188.43:8000';
 
 export const fetchBlockMetrics = async (): Promise<BlockMetrics> => {
   const response = await axios.get(`${API_BASE_URL}/block_metrics`);
